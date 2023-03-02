1c1ce51966cee078617012142df20b4a058eb253
rahul <gandhirahul190@gmail.com>
2023-03-02 13:07:24 +0000

    sisxth commit

---
+++ mysql.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+mysql -u root -pPass@123 <<EOF
+create database mydb;
+use mydb;
+create table posts( id int,status varchar(200), posturl varchar(200));
+insert into posts(id,status,posturl)values (1,"come to goa","https://insta.com/ra.jpg");
+EOF
