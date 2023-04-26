b76648cec2aa319e81bc831d96e3cedc2eabf567
rahulgandhi-1 <122810060+rahulgandhi-1@users.noreply.github.com>
2023-04-26 23:17:40 +0530

    Update mysql.sh
    fifth commit

--- mysql.sh
+++ mysql.sh
@@ -5,4 +5,5 @@ mysql -u root -pPass@123 << EOF
 create database mydb;
 use mydb;
 create table posts( id int,status varchar(200), posturl varchar(200));
-insert
+insert into posts(id,status,posturl)values (1,"come to goa","https://insta.com/ra.jpg");
+EOF
