8b694cbeed8ae3737b626c8d185a9268dd419a9b
rahulgandhi-1 <122810060+rahulgandhi-1@users.noreply.github.com>
2023-04-26 23:16:12 +0530

    Create mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+mysql -u root -pPass@123 -e "create database insta;";   #no space in -p and password here
+mysql -u root -pPass@123 << EOF
+create database mydb;
+use mydb;
+create table posts( id int,status varchar(200), posturl varchar(200));
+insert
