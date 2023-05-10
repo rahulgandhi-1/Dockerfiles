196881cf0376b47ce39943ebdda0ce53d98a25f7
rahulgandhi-1 <122810060+rahulgandhi-1@users.noreply.github.com>
2023-05-10 21:38:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_geek():
+def hello():
     return '<h1>Hello from Flask & Docker</h2>'
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
